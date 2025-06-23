0cc756524880d11261636084b8d8ba6ef0cb3b10
Veronika Dmytrenko <nikadmitrenko914@gmail.com>
2025-06-23 23:59:32 +0300

    Create Book-model in core-app

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    tittle = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    year = models.PositiveIntegerField()
+    price = models.FloatField()
+
+
+    def __str__(self):
+        return f"{self.tittle} - {self.author}"
