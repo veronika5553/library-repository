e9b6299c6e60d0c697ee8c11ced105cee215276a
Veronika Dmytrenko <nikadmitrenko914@gmail.com>
2025-06-24 17:56:04 +0300

    Modified field title and created migrations

--- core/models.py
+++ core/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class Book(models.Model):
-    tittle = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     genre = models.CharField(max_length=100)
     year = models.PositiveIntegerField()
     price = models.FloatField()
 
     def __str__(self):
-        return f"{self.tittle} - {self.author}"
+        return f"{self.title} - {self.author}"
